60d60c925300e1fe053f93b97575461b854cee53
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-02-05 23:24:05 -0300

    Add servers and chars request

--- api_calls.py
+++ api_calls.py
@@ -6,48 +6,72 @@ import re
 import aiohttp
 
 
-async def login(email, password):
-    data = {
-        "method": "signup_or_login",
-        "arguments": (
-            '{ "email": "'
-            + email
-            + '", "password": "'
-            + password
-            + '", "only_login": "true"}'
-        ),
-    }
+class Game:
+    async def get_session(self, email, password):
+        data = {
+            "method": "signup_or_login",
+            "arguments": (
+                '{ "email": "'
+                + email
+                + '", "password": "'
+                + password
+                + '", "only_login": "true"}'
+            ),
+        }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            "https://adventure.land/api/signup_or_login", data=data
-        ) as response:
-            print(response.status)
-            text_response = await response.text()
-            messages = [
-                data for data in json.loads(text_response) if "message" in data
-            ]
-            has_logged_in = any(
-                message["message"] == "Logged In!" for message in messages
-            )
-            if has_logged_in:
-                match = re.match(
-                    r"^auth=(.+?);", response.headers["Set-Cookie"]
-                )
-                if match:
-                    auth = match.group(0)
-                    return dict(
-                        user_auth=auth.split("-")[0],
-                        user_id=auth.split("-")[1],
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "https://adventure.land/api/signup_or_login", data=data
+            ) as response:
+                if response.status == 200:
+                    text_response = await response.text()
+                    messages = [
+                        data
+                        for data in json.loads(text_response)
+                        if "message" in data
+                    ]
+                    has_logged_in = any(
+                        message["message"] == "Logged In!"
+                        for message in messages
                     )
+                    if has_logged_in:
+                        match = re.match(
+                            r"^auth=(.+?);", response.headers["Set-Cookie"]
+                        )
+                        if match:
+                            auth = match.group(1)
+                            self.user_id = auth.split("-")[0]
+                            self.session_cookie = auth.split("-")[1]
+
+                            return dict(
+                                user_id=auth.split("-")[0],
+                                session_cookie=auth.split("-")[1],
+                            )
+
+    async def get_servers_and_characters(self):
+        data = {
+            "method": "servers_and_characters",
+            "arguments": "{}",
+        }
+        headers = dict(cookie=f"auth={self.user_id}-{self.session_cookie}")
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(
+                "https://adventure.land/api/servers_and_characters",
+                data=data,
+            ) as response:
+                print(response.status)
+                print(await response.text())
 
 
 async def main():
+    game = Game()
     email = os.environ["EMAIL"]
     password = os.environ["PASSWORD"]
     print(email)
-    login_data = await login(email, password)
-    print(login_data)
+    session_data = await game.get_session(email, password)
+    print(session_data)
+    servers_and_chars = await game.get_servers_and_characters()
+    print(servers_and_chars)
 
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
